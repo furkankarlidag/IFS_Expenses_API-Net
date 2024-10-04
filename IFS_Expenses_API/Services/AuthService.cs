10db58f9252fc9f2bad56cf775f0ffdaed7bb23e
Furkan Karlıdağ <furkankarlidag1@gmail.com>
2024-10-04 10:58:20 +0300

    Update AuthService.cs

--- IFS_Expenses_API/Services/AuthService.cs
+++ IFS_Expenses_API/Services/AuthService.cs
@@ -12,12 +12,12 @@ namespace IFS_Expenses_API.Services
 
         public async Task<string> GetBearerToken()
         {
-            var tokenUrl = "https://pame8xi-dev1.build.ifs.cloud/auth/realms/pame8xidev1/protocol/openid-connect/token"; 
+            var tokenUrl = "*****"; 
             var payload = new FormUrlEncodedContent(new[]
             {
             new KeyValuePair<string, string>("grant_type", "client_credentials"),
-            new KeyValuePair<string, string>("client_id", "NEXT_FRK"),
-            new KeyValuePair<string, string>("client_secret", "yEm3zJVK4ZN1IwOK4azqVgKs8JstUuhb")
+            new KeyValuePair<string, string>("client_id", "*****"),
+            new KeyValuePair<string, string>("client_secret", "*****")
 
         });
 
